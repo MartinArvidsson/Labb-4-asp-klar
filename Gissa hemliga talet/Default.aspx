80455f6dc62f85a681c277d04e5cbdbb4bf904d2
MartinArvidsson <ma223ku@lnu.student.se>
2015-02-05 12:17:55 +0100

    Im about 15% done? Can i make it in time? IDK lets try atleast!

---
+++ Gissa hemliga talet/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Gissa_hemliga_talet.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Ange ett tal mellan 1-100:<asp:TextBox ID="Guessarea" runat="server"></asp:TextBox><asp:Button ID="GuessButton" runat="server" Text="Button" OnClick="GuessButton_Click" /><br />
+
+            <asp:RequiredFieldValidator ID="Noemptytextbox" 
+            runat="server" 
+            ErrorMessage="Fältet kan inte vara tomt"
+            ControlToValidate="Guessarea" 
+            Display="Dynamic" CssClass="ErrorMessage">
+            </asp:RequiredFieldValidator>
+
+                <asp:RangeValidator ID="ValidationOfRangeTempStep" 
+                runat="server" 
+                ErrorMessage="Måste vara inom intervallet 1-100" 
+                Display="Dynamic" 
+                ControlToValidate="Guessarea" 
+                MaximumValue="100" 
+                MinimumValue="1" 
+                Type="Integer" 
+                CssClass="ErrorMessage">
+                </asp:RangeValidator>
+    <br /><asp:Label ID="PresentationArea" runat="server" Text="" Visible="false"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Gissa hemliga talet/Model/SecretNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gissa_hemliga_talet.Model
+{
+    public class SecretNumber
+    {
+        int MaxNumberOfGuesses = 7;
+        int _number;
+        List<int>  _previousGuesses;
+
+        public bool CanMakeGuess
+        {
+            get;
+        }
+
+        public int Count
+        {
+            get;
+        }
+
+        public int? Number
+        {
+            get;
+        }       
+        enum Outcome{
+            Indefinite,
+            Low,
+            High,
+            Correct,
+            NoMoreGuesses,
+            PreviousGuess,
+        }
+        public Outcome outcome
+        {
+            get;
+            set;
+        }
+
+        public  IEnumerable<int> PreviousGuesses
+        {
+            get;
+        }
+        public void Initialize(){
+
+        }
+        
+        public Outcome MakeGuess(int guess)
+        {
+            if (guess < 1 || guess > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if(guess == _number)
+            {
+                outcome = Outcome.Correct;
+            }
+            else if(guess < _number)
+            {
+                outcome = Outcome.Low;
+            }
+            else if(guess > _number)
+            {
+                outcome = Outcome.High;
+            }
+            else if(guess == _previousGuesses.Contains(guess))
+            {
+                outcome = Outcome.PreviousGuess;
+            }
+            else if(guess == MaxNumberOfGuesses)
+            {
+                outcome = Outcome.NoMoreGuesses;
+            }
+        }
+
+        public SecretNumber()
+        {
+
+        }
+
+    }
+}
